67c65e86a500a48379dfe4e20f5194e7a24611b9
GabriellyVitoria <assuncaogabrielly5@gmail.com>
2024-10-15 18:07:50 -0300

    change the way deserialize works and save files

--- server.py
+++ server.py
@@ -12,56 +12,9 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind((host, port)) # origin
 socket.listen(0) 
 
-def receive_data(conn):
-    return conn.recv(1024).decode("utf-8")
-
-def deserialize_csv(conn):
-   
-    csv_data = receive_data(conn)
-
-    with open("csv_data.csv", 'w') as file:
-        file.write(csv_data)
-
-    # convert CSV back to dictionary
-    csv_reader = csv.DictReader(io.StringIO(csv_data))  
-    list(csv_reader)[0]  
-    print(list(csv_reader)[0])
-
-def deserialize_json(conn):
-    json_data = receive_data(conn)
-
-    with open("json_data.json", 'w') as file:
-        file.write(json_data)
-
-    print(json.loads(json_data))
-
-def deserialize_xml(conn):
-    xml_data = receive_data(conn)
-    root = ET.fromstring(xml_data)
-    xml_data_desserialized = {}
-    for element in root:
-        xml_data_desserialized[element.tag] = element.text
-
-    with open("xml_data.xml", 'w') as file:
-        file.write(xml_data)
-
-    print(xml_data_desserialized)
-
-def deserialize_yaml(conn):
-    yaml_data = receive_data(conn)
-
-    with open("yaml_data.yaml", 'w') as file:
-        file.write(yaml_data)
-
-    print(yaml.safe_load(yaml_data))
-
-def deserialize_toml(conn):
-    toml_data = receive_data(conn)
-
-    with open("toml_data.toml", 'w') as file:
-        file.write(toml_data)
-
-    print(toml.loads(toml_data))
+def save_as_file(file_name, data_serialized):
+    with open(file_name, 'w') as file:
+        file.write(data_serialized)
 
 print("Server is waiting for connection...")
 
@@ -70,53 +23,42 @@ while True:
     print ("Connected by", client)
 
     # json
-    json_data = receive_data(conn)
-    print(json.loads(json_data))
+    json_data = conn.recv(1024).decode("utf-8")
+    json_data_deserialized = json.loads(json_data)
+    print(json_data_deserialized)
 
     # toml
-    toml_data = receive_data(conn)
-    print(toml.loads(toml_data))
+    toml_data = conn.recv(1024).decode("utf-8")
+    toml_data_deserialized = toml.loads(toml_data)
+    print(toml_data_deserialized)
 
     # yaml
-    yaml_data = receive_data(conn)
-    print(yaml.safe_load(yaml_data))
+    yaml_data = conn.recv(1024).decode("utf-8")
+    yaml_data_deserialized =  yaml.safe_load(yaml_data)
+    print(yaml_data_deserialized)
 
     # csv
-    csv_data = receive_data(conn)
-    # convert CSV back to dictionary
+    csv_data = conn.recv(1024).decode("utf-8")
     csv_reader = csv.DictReader(io.StringIO(csv_data))  
-    print(list(csv_reader)[0])  
+    csv_data_desserialized = list(csv_reader)[0] # convert csv back to dictionary 
+    print(csv_data_desserialized)
 
     # xml
-    xml_data = receive_data(conn)
+    xml_data = conn.recv(1024).decode("utf-8")
+
+    # convert xml back to dictionary 
     root = ET.fromstring(xml_data)
     xml_data_desserialized = {}
     for element in root:
         xml_data_desserialized[element.tag] = element.text
-
+    
     print(xml_data_desserialized)
 
-
-    with open("json_data.json", 'w') as file:
-        file.write(json_data)
-
-    with open("toml_data.toml", 'w') as file:
-        file.write(toml_data)
-
-    with open("yaml_data.yaml", 'w') as file:
-        file.write(yaml_data)
-
-    with open("csv_data.csv", 'w') as file:
-        file.write(csv_data)
-
-    with open("xml_data.xml", 'w') as file:
-        file.write(xml_data)
-
-    #deserialize_json(conn)
-    #deserialize_toml(conn)
-    #deserialize_yaml(conn)
-    #deserialize_csv(conn)
-    #deserialize_xml(conn)
+    save_as_file("json_data.json", json_data)
+    save_as_file("toml_data.toml", toml_data)
+    save_as_file("yaml_data.yaml", yaml_data)
+    save_as_file("csv_data.csv", csv_data)
+    save_as_file("xml_data.xml", xml_data)
     
     conn.close()
 
